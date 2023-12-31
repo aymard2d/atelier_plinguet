5544c06dbd5b2e6fb6483b973798355a6d55ef2d
Aymard de Dieulevveult <aymard.de.dieu@gmail.com>
2023-12-31 18:58:37 +0100

    starting atelier

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_31_113351) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_31_114958) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "furnitures", force: :cascade do |t|
+    t.string "name"
+    t.string "material"
+    t.string "color"
+    t.string "type_of_furniture"
+    t.string "description"
+    t.date "date_of_manufacture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
