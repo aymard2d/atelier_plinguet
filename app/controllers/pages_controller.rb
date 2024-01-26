93377d5f08fad14e2b7294ac89171cf43c5a2493
Aymard de Dieuleveult <aymard.de.dieu@gmail.com>
2024-01-26 18:11:41 +0100

    ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @last_creations = Furniture.last(3)
+    @last_realisations = Accessory.last(3)
   end
 end
--- app/javascript/controllers/furniture_search_filter_controller.js
+++ app/javascript/controllers/furniture_search_filter_controller.js
@@ -5,24 +5,3 @@ export default class extends Controller {
   connect() {
   }
 }
-
-// app/assets/javascripts/furniture.js
-
-document.addEventListener("DOMContentLoaded", function () {
-  const typeLinks = document.querySelectorAll(".category-search a");
-
-  typeLinks.forEach((link) => {
-    link.addEventListener("click", (event) => {
-      event.preventDefault();
-
-      const type = link.dataset.type;
-      fetch(`/furnitures/show_by_type/${type}.js`)
-        .then((response) => response.text())
-        .then((html) => {
-          document.querySelector(".galerie").innerHTML = html;
-        })
-        .catch((error) => console.error("Error fetching data:", error));
-    });
-  });
-});
-  
