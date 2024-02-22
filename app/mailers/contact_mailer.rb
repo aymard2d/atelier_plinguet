15e29355d9ed50e377a4ddf9d0c67e8fb92f8dea
Aymard de Dieuleveult <aymard.de.dieu@gmail.com>
2024-02-22 11:59:48 +0100

    test

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def new_contact_email
     @contact = params[:contact]
-    mail(to: 'aymard.de.dieu@gmail.com', subject: "Demande d'information")
+    mail(to: 'sarahandrre@gmail.com', subject: "Demande d'information")
   end
 end
