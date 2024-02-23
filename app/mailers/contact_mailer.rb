8b5f1b7e83c8912f6ccf675c6a08341879720d55
Aymard de Dieuleveult <aymard.de.dieu@gmail.com>
2024-02-23 12:55:43 +0100

    custom shits

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def new_contact_email
     @contact = params[:contact]
-    mail(to: 'sarah92370@hotmail.fr', subject: "Demande d'information")
+    mail(to: 'contact@atelier-plinguet.fr', subject: "Demande d'information")
   end
 end
