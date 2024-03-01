a336dc126d72068b0a1c085d9b9aa1b8f6c11897
Aymard de Dieuleveult <aymard.de.dieu@gmail.com>
2024-03-01 23:44:38 +0100

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_29_115421) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_01_210017) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -90,6 +90,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_29_115421) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "localisation"
+    t.string "varnish"
+    t.string "varnish_brand"
+    t.string "color"
+    t.string "paint_brand"
+    t.string "tint"
   end
 
   create_table "users", force: :cascade do |t|
