f83f5b77ae63a80df5441b32c3cca4e0f0ecfc40
Aymard de Dieuleveult <aymard.de.dieu@gmail.com>
2024-03-05 11:55:41 +0100

    ok

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,8 @@
 class Contact < ApplicationRecord
-  validates :name, :email, :message, :subject, presence: true
+  validates :name, presence: {presence: true, message: "(votre nom) doit être renseigné"}, length: { minimum: 2, message: "(votre nom) doit contenir au moins deux caractères."}
+  validates :email, presence: {presence: true, message: ": merci de renseigner votre email." }
+  validates :message, presence: {presence: true, message: ": merci de rédiger votre message" }, length: { minimum: 10, message: "doit contenir au moins 10 caractères" }
+  validates :subject, presence: {presence: true, message: ": merci de sélectionner un sujet." }
+  #length: { minimum: 2, message: "Votre nom doit contenir au moins deux caractères" }
+
 end
